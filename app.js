fd0578b9446133551038e845cdc700285ceff192
Akshayshah47 <removeit61@gmail.com>
2023-06-12 10:17:32 +0530

    added add-product route and with post method

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express= require('express');
 const app=express();
-app.use((req,res,next)=>{
-    console.log("in first middleware");
-    next();
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+    //it is used to send response to the client. After sending response, middle ware written behind will not execute
+    res.send('<form action="/product" method="post"><label for="product">Product</label></label><input type="text" name="title" id="product"><br>Quantity:<input type="number" name="quantity" id="quant"><button type="submit">Add Product</button></form>');
 })
-app.use((req,res,next)=>{
-    console.log("in the second middle ware");
-    res.send('<h2>hlo this is from express js app</h2>')
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
 })
+app.use('/',(req,res,next)=>{
+    res.send("<h2>this is root route</h2>")
+})
+//it will do two work of node js. 1) create servr and send selp as callback 2)listen to the port
 app.listen(3000);
